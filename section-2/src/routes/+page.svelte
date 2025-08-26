be4e955e8b4f9e89611397125d1f61aae761fd15
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-08-25 20:40:30 -0500

    Reactivity class

---
+++ section-2/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  let number = $state(0);
+  //   let userInformation = $derived(
+  //     number === 0
+  //       ? "Hey why don't you try clicking on the button?"
+  //       : `you've clicked ${number} times`
+  //   );
+
+  const calculateUserInformation = (number: number) => {
+    if (number === 0) {
+      return "Hey why don't you try clicking on the button?";
+    }
+
+    if (number === 1) {
+      return "you've clicked one times";
+    }
+
+    return `you've clicked ${number} times`;
+  };
+
+  let userInformation = $derived.by(() => calculateUserInformation(number));
+
+  const onclick = () => {
+    number++;
+  };
+</script>
+
+<button {onclick}> Click Me! </button>
+
+<h1>{number}</h1>
+<p>{userInformation}</p>
+<div class="container">
+  <p>This is the left side</p>
+  <p>This is the right side</p>
+</div>
+
+<style>
+  h1 {
+    color: blue;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+  }
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
