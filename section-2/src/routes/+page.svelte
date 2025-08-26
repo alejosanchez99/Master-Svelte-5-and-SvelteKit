b1621e2676f1c0e06901d05b8a537f3e86ed1705
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-08-26 12:13:15 -0500

    Snippets, children, each.

--- section-2/src/routes/+page.svelte
+++ section-2/src/routes/+page.svelte
@@ -1,8 +1,24 @@
-<script>
-  import UserInput from "$lib/components/UserInput.svelte";
+<script lang="ts">
+  //   import UserInput from "$lib/components/UserInput.svelte";
+
+  let peopleWaiting = $state(["1232", "23233", "23232 "]);
 </script>
 
-<UserInput userName="Niklas">
+<h1>Waiting queue</h1>
+{#snippet userInput(personId: string)}
+  <li>ID for person waiting: {personId}</li>
+{/snippet}
+
+<ul>
+  {#each peopleWaiting as person}
+    {@render userInput(person)}
+  {/each}
+</ul>
+
+<button onclick={() => peopleWaiting.push(new Date().getTime().toString())}>
+  I'm waiting too!
+</button>
+
+<!-- <UserInput userName="Niklas">
   <h1>This is the stuff passed inside</h1>
-  </UserInput
->
+</UserInput> -->
