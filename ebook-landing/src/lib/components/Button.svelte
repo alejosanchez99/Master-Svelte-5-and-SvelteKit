d5476c47da8a1361ee643d5108035a667cfe43ba
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-08-28 20:47:23 -0500

    Add Stripe integration for checkout process and success/failure pages

--- ebook-landing/src/lib/components/Button.svelte
+++ ebook-landing/src/lib/components/Button.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
+  import { PUBLIC_STRIPE_KEY } from "$env/static/public";
+  import { loadStripe } from "@stripe/stripe-js";
   import type { Snippet } from "svelte";
   import type { HTMLButtonAttributes } from "svelte/elements";
 
   type Button = HTMLButtonAttributes & {
     children: Snippet;
   };
+
+  const onclick = async () => {
+    const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
+
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { sessionId } = await response.json();
+
+    stripe?.redirectToCheckout({ sessionId });
+  };
+
   let { children, ...props }: Button = $props();
 </script>
 
-<button {...props}>
+<button {...props} {onclick}>
   {@render children()}
 </button>
 
