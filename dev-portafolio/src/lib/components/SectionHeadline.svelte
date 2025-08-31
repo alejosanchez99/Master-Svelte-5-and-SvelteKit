b3d3e0ea781a2df676369a788c955524f2df98a8
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-08-31 16:07:57 -0500

    feat: add AboutMeSection component with content and styling; include SectionHeadline and image

---
+++ dev-portafolio/src/lib/components/SectionHeadline.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface HeadlineSectionProps {
+    children: Snippet;
+    sectionName: string;
+  }
+  let { children, sectionName }: HeadlineSectionProps = $props();
+</script>
+
+<div class="default-margin" id={sectionName}>
+  <h2>{@render children()}</h2>
+  <div class="underscore"></div>
+</div>
