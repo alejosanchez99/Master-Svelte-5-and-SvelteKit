9dc0bb368afda86aa502531a2f1a4a89136a8c9c
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-08-31 15:50:05 -0500

    feat: add Button and HeroSection components with styling and functionality

--- dev-portafolio/src/lib/components/sections/HeroSection.svelte
+++ dev-portafolio/src/lib/components/sections/HeroSection.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+
+  const onclick = () => {
+    goto("/#contact-form");
+  };
 </script>
 
 <section class="hero-section default-margin">
   <h3>Hello! I'm Nick</h3>
   <div class="underscore mb-m"></div>
   <h1>I'm a Svelte developer crafting intuitive and performant</h1>
-  <h1 class="dark-grey">Web experiences.</h1>
-  <button>Let's talk!</button>
+  <h1 class="dark-grey mb-m">Web experiences.</h1>
+  <Button {onclick}>Let's talk!</Button>
 </section>
 
 <style>
